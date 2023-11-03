ec41a630e22c7212f02932e4b52f148981754c58
probably-ARD <durakov.sasha95@mail.ru>
2023-11-03 16:55:53 +0300

    all core methods

--- password_manager/model/core.py
+++ password_manager/model/core.py
@@ -15,16 +15,49 @@ class passwordStorage:
 
         logger.addHandler(handler)
         
-        logger.info('start core')
+        logger.info(f'start core\npath: {external_storage_path}')
 
         # check parameters
-        if not isinstance(external_storage_path, str):
-            raise TypeError('external_storage_path must be of the str type')
+        self.__check_params(
+            [external_storage_path],
+            [str]
+        )
+
+        # reading storage
+        try:
+            with open(external_storage_path, 'r', encoding='utf-8') as f:
+                storage = dict(json.load(f))
+        except Exception as e:
+            logger.fatal(f'error with reading storage (__init__)\n{e}', stack_info=True)
         
         self.__logger = logger
         self.__external_storage_path = external_storage_path
-    
-    
+        self.__storage = storage
+
+
+    def __save_storage(self, storage: dict) -> None:
+        '''
+        simple writing storage
+        '''
+        try:
+            with open(self.__external_storage_path, 'w', encoding='utf-8') as f:
+                json.dump(storage, f)
+        except Exception as e:
+            self.__logger.fatal(f'error with writing storge\nstorage: {storage}\n{e}', stack_info=True)
+
+
+    def __check_params(self,
+            params: list,
+            params_types: list
+    ):
+        '''
+        func to simple params cheking
+        '''
+        for i in range(len(params)):
+            if not isinstance(params[i], params_types[i]):
+                raise TypeError(f'{params[i]} must be of the {params_types[i]} type')
+
+
     def add_app(self, 
                 app: str, 
                 login: str, 
@@ -37,28 +70,16 @@ class passwordStorage:
         self.__logger.info('adding app')
 
         # check parameters
-        if not isinstance(app, str):
-            raise TypeError('app must be of the STR type')
+        self.__check_params(
+            [app, login, password, add_info],
+            [str, str, (str, type(None)), (str, type(None))]
+        )
         
-        if not isinstance(login, str):
-            raise TypeError('login must be of the STR type')
-        
-        if not isinstance(password, (str, type(None))):
-            raise TypeError('password must be of the STR type')
-        
-        if not isinstance(add_info, (str, type(None))):
-            raise TypeError('add_info must be of the STR type')
-        
-        # read storage
-        try:
-            with open(self.__external_storage_path, 'r', encoding='utf-8') as f:
-                storage = json.load(f)
-        except Exception as e:
-            self.__logger.fatal(f'error with reading storage (add_app)\n{e}', stack_info=True)
+        storage = self.__storage
 
         # adding storage
         try:
-            storage[f'{app} - {login}'] = {
+            storage[f'{app}-{login}'] = {
                 "app": app,
                 "login": login,
                 "password": password,
@@ -68,28 +89,87 @@ class passwordStorage:
             self.__logger.fatal(f'error with adding storage (add_app)\n{e}', stack_info=True)
 
         # write storage
-        try:
-            with open(self.__external_storage_path, 'w', encoding='utf-8') as f:
-                json.dump(storage, f)
-        except Exception as e:
-            self.__logger.fatal(f'error with writing storage (add_app)\ncheck the availability of the storage and the json dict written in it\n{e}', stack_info=True)
+        self.__save_storage(storage)
+        self.__storage = storage
 
-        return storage[f'{app} - {login}']
+        return storage
 
 
     def get_storage(self) -> dict:
         '''
         get dict with storage
         '''
-        try:
-            with open(self.__external_storage_path, 'r', encoding='utf-8') as f:
-                storage = json.load(f)
-        except Exception as e:
-            self.__logger.fatal(f'error with reading storage (get_storage)\n{e}', stack_info=True)
+        storage = self.__storage
         
+        self.__logger.info(f'getting storage {storage}')
         return storage
 
-password_storage = passwordStorage(r'A:\password_storage.json')
-password_storage.add_app('1app', 'login', add_info='1 add info')
-password_storage.add_app('2app', 'login', add_info='2 add info')
-print(password_storage.get_storage())
+    
+    def edit_storage(self,
+            app_name: str,
+            **kwargs: str
+    ) -> None:
+        '''
+        edit storage\n
+        **kwargs: app, login, password, add_info: all str type
+        '''
+        # check parameter
+        self.__check_params([app_name], [str])
+        
+        # create editing app and check kwargs with adding it in editing app
+        editing_app = {'app': '', 'login': '', 'password': '', 'add_inf': ''}
+        
+        if 'app' in kwargs:
+            self.__check_params([kwargs['app']], [str])
+            editing_app['app'] = kwargs['app']
+        else:
+            editing_app['app'] = ''
+        
+        if 'login' in kwargs:
+            self.__check_params([kwargs['login']], [str])
+            editing_app['login'] = kwargs['login']
+        else:
+            editing_app['login'] = ''
+        
+        if 'password' in kwargs:
+            self.__check_params([kwargs['password']], [str])
+            editing_app['password'] = kwargs['password']
+        else:
+            editing_app['password'] = ''
+
+        if 'add_info' in kwargs:
+            self.__check_params([kwargs['add_info']], [str])
+            editing_app['add_info'] = kwargs['add_info']
+        else:
+            editing_app['add_info'] = ''
+                
+        # reading storage
+        old_storage = self.__storage
+
+        # edited storage
+        edited_app = old_storage[app_name]
+        # delete old app
+        old_storage.pop(app_name)
+
+        for key, data in editing_app:
+            if data != '':
+                edited_app[key] = data
+        
+        self.add_app(
+            app=edited_app['app'], 
+            login=edited_app['login'],
+            password=edited_app['password'],
+            add_info=edited_app['add_info']
+        )
+        
+
+    def del_app(self, app_name: str) -> None:
+        '''
+        delete app from storage
+        '''
+        self.__check_params([app_name], [str])
+        storage = self.__storage
+
+        storage.pop(app_name)
+
+        self.__save_storage(storage)
