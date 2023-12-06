4d62d80ef263d819adb379a955dd16fc36da4e2c
probably-ARD <durakov.sasha95@mail.ru>
2023-12-07 00:22:45 +0300

    main_win

--- password_manager/view/wins.py
+++ password_manager/view/wins.py
@@ -3,31 +3,64 @@ from password_manager.view import wins_config
     
 
 class mainWin(tk.Tk):
-    def __init__(self, apps_list: list):
-        if not isinstance(apps_list, list):
-            raise ValueError('arg apps_list must be a list type')
-
+    '''
+    password storage main win
+    '''
+    def __init__(self):
         super().__init__()
 
-        self.__conf = wins_config.MAIN_WIN_CONF
+        conf = wins_config.MAIN_WIN_CONF
 
         # win confs
-        self.geometry("600x300+600+300")
+        self.geometry("600x305+600+300")
         self.resizable(False, False)
-        self.title(self.__conf['win_title'])
-        
+        self.title(conf['win_title'])
+
+        # menu
+        self.menu = tk.Menu(self, tearoff=0)
+        self.config(menu=self.menu)
+
+        file_menu = tk.Menu(self.menu, tearoff=0)
+        file_menu.add_command(label=conf['open_storage'])
+        file_menu.add_command(label=conf['new_storage'])
+        file_menu.add_command(label=conf['save_storage'])
+
+        self.menu.add_cascade(label=conf['file_menu'], menu=file_menu)
+
+        self.menu.add_command(label=conf['about_prog'])
+
         # listbox with apps
         self.apps_listBox = tk.Listbox(self, width=70, height=18)
         self.apps_listBox.grid(column=1, row=1, padx=5, pady=5)
-        for item in apps_list:
-            self.apps_listBox.insert(tk.END, item)
-        
+
         self.btns_frame = tk.Frame(self, width=50, height=4)
         self.btns_frame.grid(column=2, row=1, padx=5, pady=5)
-        
+
         # btns
-        self.check_btn = tk.Button(self.btns_frame, text=self.__conf['view_btn_caption'], width=20, height=2)
+        self.check_btn = tk.Button(self.btns_frame, text=conf['view_btn_caption'], width=20, height=2)
         self.check_btn.grid(row=1, pady=5)
 
-        self.add_btn = tk.Button(self.btns_frame, text=self.__conf['add_btn_caption'], width=20, height=2)
+        self.add_btn = tk.Button(self.btns_frame, text=conf['add_btn_caption'], width=20, height=2)
         self.add_btn.grid(row=2, pady=5)
+
+        self.change_btn = tk.Button(self.btns_frame, text=conf['change_btn_caption'], width=20, height=2)
+        self.change_btn.grid(row=3, pady=5)
+
+        self.del_btn = tk.Button(self.btns_frame, text=conf['del_btn_caption'], width=20, height=2)
+        self.del_btn.grid(row=4, pady=5)
+
+
+class chekWin(tk.Tk):
+    def __init__(self, 
+                 login: str, 
+                 password: str,
+                 data: list
+                 ):
+        super().__init__()
+
+        conf = wins_config.CHECK_WIN_CONF
+
+        # win confs
+        self.geometry("400x205+600+300")
+        self.resizable(False, False)
+        self.title(conf['win_title'])
--- password_manager/view/wins_config.py
+++ password_manager/view/wins_config.py
@@ -1,6 +1,18 @@
 MAIN_WIN_CONF = {
     "win_title": "Хранилище паролей",
-    "view_btn_caption": "посмореть",
+    "view_btn_caption": "просмореть",
     "add_btn_caption": "добавить",
-    "del_btn_caption": "удалить"
+    "change_btn_caption": "изменить",
+    "del_btn_caption": "удалить",
+    "file_menu": "файл",
+    "open_storage": "открыть хранилище",
+    "new_storage": "новое хранилище",
+    "save_storage": "сохранить",
+    "about_prog": "о программе"
+}
+
+CHECK_WIN_CONF = {
+    "win_title": "просмотр данных",
+    "copy_password_btn_caption": "пароль",
+    "copy_login_btn_caption": "логин"
 }
